b19329cbeb57b5002c994495465fe5ee540d38c7
PersonablePhaniK <personablephani@aol.com>
2020-02-28 19:56:06 +0530

    configured Okta-auth and Docker, docker-compose

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import Staff from "./components/pages/Staff";
+import Login from "./components/auth/Login";
+
+import "./App.css";
+
+function onAuthRequired({ history }) {
+  history.push("/login");
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Security
+          issuer="https://dev-441752.okta.com/oauth2/default"
+          clientId="0oa2p2u1bZpalmWdQ4x6"
+          
+
+          redirectUri={window.location.origin + "/implicit/callback"}
+          onAuthRequired={onAuthRequired}
+          pkce={true}
+        >
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Route path="/" exact={true} component={Home} />
+              <SecureRoute path="/staff" exact={true} component={Staff} />
+              <Route
+                path="/login"
+                render={() => <Login baseUrl="https://dev-441752.okta.com" />}
+              />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+            </div>
+          </div>
+        </Security>
+      </Router>
+    );
+  }
+}
+
+export default App;

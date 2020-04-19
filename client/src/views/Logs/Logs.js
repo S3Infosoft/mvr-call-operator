89631c548ca38931861c6400a1a9d1614c7ecbec
PersonablePhaniK <personablephani@aol.com>
2020-04-19 07:09:29 +0530

    download filtered reports as excel/pdf file

--- client/src/views/Logs/Logs.js
+++ client/src/views/Logs/Logs.js
@@ -5,6 +5,11 @@ import { fetchLogs } from "../../actions/authActions";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import moment from "moment";
+import { ExportCSV } from "../../utils/ExportCSV";
+import Doc from "../../utils/DocService";
+import PdfContainer from "../../utils/PdfContainer";
+import ReactHTMLTableToExcel from "react-html-table-to-excel";
+import "./Logs.css";
 
 // import usersData from "./UsersData";
 
@@ -21,8 +26,8 @@ function LogRow(props) {
       <td>{log.name + " "}</td>
       <td>{log.caller_number}</td>
       <td>{log.state}</td>
-      <td>{log.start_time}</td>
-      <td>{log.end_time}</td>
+      <td>{moment.unix(log.start_time).format("MMMM D YYYY HH:mm")}</td>
+      <td>{moment.unix(log.end_time).format("MMMM D YYYY HH:mm")}</td>
       <td>{log.duration}</td>
       <td>{log._ds + " "}</td>
     </tr>
@@ -35,6 +40,7 @@ class Logs extends Component {
 
     this.state = {
       selection: [],
+      fileName: "Call_Logs_Report",
     };
   }
 
@@ -49,12 +55,8 @@ class Logs extends Component {
       let dataObj = {};
       dataObj["caller_number"] = vals[i]._source.caller_number;
       dataObj["state"] = vals[i]._source.state;
-      dataObj["start_time"] = moment(vals[i]._source.start_time).format(
-        "MMM D, YYYY, HH:mmA"
-      );
-      dataObj["end_time"] = moment(vals[i]._source.end_time).format(
-        "MMM D, YYYY, HH:mmA"
-      );
+      dataObj["start_time"] = vals[i]._source.start_time;
+      dataObj["end_time"] = vals[i]._source.end_time;
       dataObj["duration"] = vals[i]._source.duration;
 
       if (vals[i]._source.log_details !== []) {
@@ -69,6 +71,16 @@ class Logs extends Component {
       dataArry.push(dataObj);
     }
 
+    let selected = this.state.selection;
+
+    console.log(selected);
+    if (selected.length == 2) {
+      dataArry = dataArry.filter(
+        (item) =>
+          item.start_time >= selected[0] && item.start_time <= selected[1]
+      );
+    }
+
     return dataArry;
   }
 
@@ -80,8 +92,8 @@ class Logs extends Component {
     let firstday = new Date(curr.setDate(first)).toUTCString();
     let lastday = new Date(curr.setDate(last)).toUTCString();
 
-    let Monday = moment(firstday).format("MMM D, YYYY, HH:mmA");
-    let Sunday = moment(lastday).format("MMM D, YYYY, HH:mmA");
+    let Monday = moment(firstday).unix();
+    let Sunday = moment(lastday).unix();
 
     return [Monday, Sunday];
   }
@@ -95,8 +107,8 @@ class Logs extends Component {
     let firstday = new Date(today.setDate(first)).toUTCString();
     let lastday = new Date(today.setDate(last)).toUTCString();
 
-    let Monday = moment(firstday).format("MMM D, YYYY, HH:mmA");
-    let Sunday = moment(lastday).format("MMM D, YYYY, HH:mmA");
+    let Monday = moment(firstday).unix();
+    let Sunday = moment(lastday).unix();
 
     return [Monday, Sunday];
   }
@@ -107,8 +119,8 @@ class Logs extends Component {
     let first = new Date(date.getFullYear(), date.getMonth(), 1);
     let last = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
-    let firstDay = moment(first).format("MMM D, YYYY, HH:mmA");
-    let lastDay = moment(last).format("MMM D, YYYY, HH:mmA");
+    let firstDay = moment(first).unix();
+    let lastDay = moment(last).unix();
 
     return [firstDay, lastDay];
   }
@@ -119,8 +131,8 @@ class Logs extends Component {
     let prevStartDate = new Date(date.getFullYear(), date.getMonth() - 1, 1);
     let preEndDate = new Date(date.getFullYear(), date.getMonth() - 1 + 1, 0);
 
-    let firstDay = moment(prevStartDate).format("MMM D, YYYY, HH:mmA");
-    let lastDay = moment(preEndDate).format("MMM D, YYYY, HH:mmA");
+    let firstDay = moment(prevStartDate).unix();
+    let lastDay = moment(preEndDate).unix();
 
     return [firstDay, lastDay];
   }
@@ -146,65 +158,77 @@ class Logs extends Component {
     return activeSelection;
   };
 
+  createPdf = (html) => Doc.createPdf(html);
+
   render() {
     let data = this.props.auth.data;
 
     let dataArry = this.getData(data);
 
-    let selected = this.state.selection;
-    console.log(selected);
-    if (selected.length == 2) {
-      dataArry = dataArry.filter(
-        (item) =>
-          item.start_time >= selected[0] && item.start_time <= selected[1]
-      );
-    }
-
     return (
       <div className="animated fadeIn">
         <Row>
           <Col xl={12}>
-            <Card>
-              <CardHeader>
-                <i className="fa fa-align-justify"></i> CallLogs{" "}
-                <span>
-                  <select
-                    name="reportList"
-                    id="reportSelection"
-                    onChange={this.dataSelection}
-                  >
-                    <option value="0">Complete Report</option>
-                    <option value="1">Current Week</option>
-                    <option value="2">Last Week</option>
-                    <option value="3">Current Month</option>
-                    <option value="4">Last Month</option>
-                  </select>
-                </span>
-                {/* <small className="text-muted">example</small> */}
-              </CardHeader>
-              <CardBody>
-                <Table responsive hover>
-                  <thead>
-                    <tr>
-                      <th scope="col">S.No</th>
-                      <th scope="col">User_ID</th>
-                      <th scope="col">Name</th>
-                      <th scope="col">Caller Number</th>
-                      <th scope="col">State</th>
-                      <th scope="col">Start time</th>
-                      <th scope="col">End time</th>
-                      <th scope="col">Duration</th>
-                      <th scope="col">_ds</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {dataArry.map((log, index) => (
-                      <LogRow key={index} log={log} index={index} />
-                    ))}
-                  </tbody>
-                </Table>
-              </CardBody>
-            </Card>
+            <PdfContainer createPdf={this.createPdf}>
+              <span
+                style={{
+                  position: "absolute",
+                  right: "1rem",
+                  top: "0.3rem",
+                }}
+              >
+                {/* <ExportCSV  csvData={dataArry} fileName={this.state.fileName} /> */}
+                <ReactHTMLTableToExcel
+                  className="btn btn-success"
+                  table="logsReport"
+                  filename="Call_logs_Report"
+                  sheet="Sheet"
+                  buttonText="Export to excel"
+                />
+              </span>
+              <Card>
+                <CardHeader>
+                  <i className="fa fa-align-justify"></i> CallLogs{" "}
+                  <span style={{ position: "absolute", right: "3rem" }}>
+                    Select your Report :{" "}
+                    <select
+                      name="reportList"
+                      id="reportSelection"
+                      onChange={this.dataSelection}
+                    >
+                      <option value="0">Complete Report</option>
+                      <option value="1">Current Week</option>
+                      <option value="2">Last Week</option>
+                      <option value="3">Current Month</option>
+                      <option value="4">Last Month</option>
+                    </select>
+                  </span>
+                  {/* <small className="text-muted">example</small> */}
+                </CardHeader>
+                <CardBody>
+                  <Table responsive hover id="logsReport">
+                    <thead>
+                      <tr>
+                        <th scope="col">S.No</th>
+                        <th scope="col">User_ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Caller Number</th>
+                        <th scope="col">State</th>
+                        <th scope="col">Start time</th>
+                        <th scope="col">End time</th>
+                        <th scope="col">Duration</th>
+                        <th scope="col">_ds</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {dataArry.map((log, index) => (
+                        <LogRow key={index} log={log} index={index} />
+                      ))}
+                    </tbody>
+                  </Table>
+                </CardBody>
+              </Card>
+            </PdfContainer>
           </Col>
         </Row>
       </div>
